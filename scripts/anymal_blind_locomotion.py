9d284ce8ebc8a8093de35403329751f48201ac18
fanshi14 <shifan@jsk.imi.i.u-tokyo.ac.jp>
2020-03-13 13:01:09 +0100

    Add testing for trained model follow cartpole example.

--- scripts/anymal_blind_locomotion.py
+++ scripts/anymal_blind_locomotion.py
@@ -13,6 +13,11 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('--cfg', type=str, default=os.path.abspath(__RSCDIR__ + "/default_cfg.yaml"),
                     help='configuration file')
+parser.add_argument('-m', '--mode', help='set mode either train or test', type=str, default='train')
+parser.add_argument('-w', '--weight', help='trained weight path', type=str, default='')
+args = parser.parse_args()
+mode = args.mode
+
 cfg_abs_path = parser.parse_args().cfg
 cfg = YAML().load(open(cfg_abs_path, 'r'))
 
@@ -23,33 +28,58 @@ saver = ConfigurationSaver(log_dir=log_dir+'/ANYmal_blind_locomotion',
                            save_items=[rsg_root+'raisim_gym/env/env/ANYmal/Environment.hpp', cfg_abs_path])
 
 # create environment from the configuration file
+if args.mode == "test": # for test mode, force # of env to 1
+    cfg['environment']['num_envs'] = 1
 env = Environment(RaisimGymEnv(__RSCDIR__, dump(cfg['environment'], Dumper=RoundTripDumper)))
 
 # Get algorithm
-model = PPO2(
-    tensorboard_log=saver.data_dir,
-    policy=MlpPolicy,
-    policy_kwargs=dict(net_arch=[dict(pi=[128, 128], vf=[128, 128])]),
-    env=env,
-    gamma=0.998,
-    n_steps=math.floor(cfg['environment']['max_time'] / cfg['environment']['control_dt']),
-    ent_coef=0,
-    learning_rate=1e-3,
-    vf_coef=0.5,
-    max_grad_norm=0.5,
-    lam=0.95,
-    nminibatches=1,
-    noptepochs=10,
-    cliprange=0.2,
-    verbose=1,
-)
+if mode == 'train':
+    model = PPO2(
+        tensorboard_log=saver.data_dir,
+        policy=MlpPolicy,
+        policy_kwargs=dict(net_arch=[dict(pi=[128, 128], vf=[128, 128])]),
+        env=env,
+        gamma=0.998,
+        n_steps=math.floor(cfg['environment']['max_time'] / cfg['environment']['control_dt']),
+        ent_coef=0,
+        learning_rate=1e-3,
+        vf_coef=0.5,
+        max_grad_norm=0.5,
+        lam=0.95,
+        nminibatches=1,
+        noptepochs=10,
+        cliprange=0.2,
+        verbose=1,
+    )
 
-# tensorboard
-# Make sure that your chrome browser is already on.
-TensorboardLauncher(saver.data_dir + '/PPO2_1')
+    # tensorboard
+    # Make sure that your chrome browser is already on.
+    TensorboardLauncher(saver.data_dir + '/PPO2_1')
 
-# PPO run
-model.learn(total_timesteps=400000000, eval_every_n=50, log_dir=saver.data_dir, record_video=cfg['record_video'])
+    # PPO run
+    model.learn(total_timesteps=400000000, eval_every_n=50, log_dir=saver.data_dir, record_video=cfg['record_video'])
 
-# Need this line if you want to keep tensorflow alive after training
-input("Press Enter to exit... Tensorboard will be closed after exit\n")
+    # Need this line if you want to keep tensorflow alive after training
+    input("Press Enter to exit... Tensorboard will be closed after exit\n")
+
+else:
+    weight_path = args.weight
+    if weight_path == "":
+        print("Can't find trained weight, please provide a trained weight with --weight switch\n")
+    else:
+        print("Loaded weight from {}\n".format(weight_path))
+        model = PPO2.load(weight_path)
+    obs = env.reset()
+    running_reward = 0.0
+    ep_len = 0
+    for _ in range(100000):
+        env.wrapper.showWindow()
+        action, _ = model.predict(obs)
+        obs, reward, done, infos = env.step(action, visualize=True)
+        running_reward += reward[0]
+        ep_len += 1
+        if done[0]:
+            print("Episode Reward: {:.2f}".format(running_reward))
+            print("Episode Length", ep_len)
+            running_reward = 0.0
+            ep_len = 0
